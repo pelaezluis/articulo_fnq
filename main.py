ecd88e2a59c26ddee04dbe55e4b0042dd594ac42
pelaezluis <luispelaezbedoya@gmail.com>
2022-09-12 20:27:24 -0500

    avance 12/09/2022

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from modules.data_manipulate import EnergyAndDistances
 input_file = Env.path_data
 residue = 'FAD603'
 fnq = 'F02'
-mode = 'Ap'
+mode = 'A'
 
 print(f'Reading data from {input_file}...')
 data = EnergyAndDistances(input_file)
 # print(data.extract_energy_distances(residue))
 print(f'Extracting {fnq}...')
-print(data.extract_energy_distances_by_residue_and_fnq(residue, fnq))
+#print(data.extract_distances())
+#print(data.extract_distances_by_fnq(fnq))
+#print(data.extract_distances_by_fnq_and_mode(fnq=fnq, mode=mode))
+print(data.distances_in_interval_by_fnq(fnq,3, 5, 3))
+#print(data.extract_energy_distances_by_residue('FAD603'))
+#print(data.extract_energy_distances_by_residue_and_fnq(residue, fnq, 'GOL'))
--- modules/data_manipulate.py
+++ modules/data_manipulate.py
@@ -17,22 +17,116 @@ class EnergyAndDistances:
     def show_data(self):
         print(self.load())
 
+    ############################# ONLY DISTANCES ####################################
+
+    def extract_distances(self, output_file: str = ''):
+        """
+            Extract only the distances for all fnqs
+        """
+        df = self.load()
+        df_ = df[['FNQ', 'Mode', 'c3a', 'c4a', 'c8a', 'c9a']]
+        if output_file != '':
+            print(f'Saving {output_file}...')
+            df_.to_csv(output_file, index=False)
+        return df_
+
+
+    def extract_distances_by_fnq(self, fnq: str, output_file: str = ''):
+        """
+            Extract only the distances for all fnqs
+        """
+        df = self.extract_distances()
+        df_ = df.loc[df['FNQ'] == fnq]
+        if output_file != '':
+            print(f'Saving {output_file}...')
+            df_.to_csv(output_file, index=False)
+        return df_
+
+    
+    def extract_distances_by_fnq_and_mode(self, fnq: str, mode: str, output_file: str = ''):
+        """
+            Extract only the distances for all fnqs
+        """
+        df = self.extract_distances_by_fnq(fnq=fnq)
+        df_ = df.loc[df['Mode'] == mode]
+        if output_file != '':
+            print(f'Saving {output_file}...')
+            df_.to_csv(output_file, index=False)
+        return df_
+
+
+    def verify_intervals(self, distance_list: list, min: float, max: float, condition: int = 1):
+        counter: int = 0
+        for distance in distance_list:
+            if min <= distance <= max:
+                counter += 1
+        if counter == condition:
+            return counter
+
+
+
+    def count_distances_in_interval(self, min: float, max: float, condition: int = 1):
+        """
+            Calculate percentage of data in interval for all the fnq
+        """
+        df = self.extract_distances()
+        df_ = df.iloc[:, 2:].apply(lambda x: self.verify_intervals(x, min, max, condition), axis=1)
+        return round((df_.count() * 100 ) / len(df_), 3)
+
+    
+    def distances_in_interval_by_fnq(self, fnq: str, min: float, max: float, condition: int = 1):
+        """
+            Calculate percentage of data in interval for an specific fnq
+        """
+        df = self.extract_distances_by_fnq(fnq=fnq)
+        df_ = df.iloc[:, 2:].apply(lambda x: self.verify_intervals(x, min, max, condition), axis=1)
+        counter = 0
+        for x in list(df_):
+        
+            if x == condition:
+                counter += 1
+             
+        return round((counter * 100 ) / len(df_), 3)
+
+    
+    ############################# ENERGIES AND DISTANCES ####################################
+
 
     def extract_energies_distances_by_fnq(self, fnq: str):
+        """
+            Extract all the energies and distances for a given fnq
+        """
         df = self.load()
         return df.loc[df['FNQ'] == fnq]
     
 
     def extract_energies_distances_by_fnq_and_mode(self, fnq: str, mode: str):
+        """
+            Extract all the energies and distances for the fnq and mode given
+        """
         df = self.load()
         return df.loc[(df['FNQ'] == fnq) & (df['Mode'] == mode)]
 
     
-    def extract_energy_distances_by_residue(self, residue: str):
+    def extract_energy_distances_by_residue(self, residue: str, output_file: str = ''):
+        """
+            Extract a residue energy for all the fnqs
+        """
         df = self.load()
-        return df[['FNQ', 'Mode', residue, 'c3a', 'c4a', 'c8a', 'c9a']]
+        df_ = df[['FNQ', 'Mode', residue, 'c3a', 'c4a', 'c8a', 'c9a']]
+        if output_file != '':
+            print(f'Saving {output_file}...')
+            df_.to_csv(output_file, index=False)
+        return df_
 
     
-    def extract_energy_distances_by_residue_and_fnq(self, residue: str, fnq: str):
+    def extract_energy_distances_by_residue_and_fnq(self, residue: str, fnq: str, output_file: str = ''):
+        """
+            Extract energy only for a residue and fnq
+        """
         df = self.extract_energies_distances_by_fnq(fnq)
-        return df[['FNQ', 'Mode', residue, 'c3a', 'c4a', 'c8a', 'c9a']]
+        df_ = df[['FNQ', 'Mode', residue, 'c3a', 'c4a', 'c8a', 'c9a']]
+        if output_file != '':
+            print(f'Saving {output_file}...')
+            df_.to_csv(output_file, index=False)
+        return df_
